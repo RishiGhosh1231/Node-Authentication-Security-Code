8fdbf70a292007f9829346eb1391b91581eb14ea
Rishi Ghosh <68550739+RishiGhosh1231@users.noreply.github.com>
2021-04-02 02:21:17 +0530

    Level 2 Encryption & Environment Variables

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
@@ -9,11 +11,12 @@ app.set('view engine', 'ejs');
 
 mongoose.connect('mongodb://localhost:27017/userDB', {useNewUrlParser: true});
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
 
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 const User = mongoose.model('User', userSchema);
 
 app.get('/', (req, res)=>{
